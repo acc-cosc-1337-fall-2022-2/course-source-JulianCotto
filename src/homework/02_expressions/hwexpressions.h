b9e7c2aee35e999e14f7164bbb4397a8319ea5fd
juliancotto <julian.cotto@g.austincc.edu>
2022-09-09 14:12:04 -0500

    hw-02-expressions

--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -1,8 +1,13 @@
-﻿//example
+﻿const double tax_rate = .0675;
+
+//example
 int add_numbers(int num1, int num2);
 
 //write function prototype here
-
+double get_sales_tax_amount(double meal_amount);
+double get_tip_amount(double meal_amount, double tip_rate);
+double convert_to_decimal(double tip);
+void display_receipt(double meal_amount, double tax_amount, double tip_amount, double total);
 
 
 
