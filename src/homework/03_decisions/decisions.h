32a98291fcb4446fd25953e12506baa894c6211e
JulianCotto <julian.cotto@g.austincc.edu>
2022-09-24 01:13:10 -0500

    Update decisions.h

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,7 +1,15 @@
-//write include required statements
+#include <string>
 
+using std::string;
 
-//Write a function(s) prototype  here
+string get_letter_grade_using_if(int grade);
+string get_letter_grade_using_switch(int grade);
+
+int menu();
+void menu_switch_functions(int selx);
+void loading_bar();
+void outro();
+
 
 
 
