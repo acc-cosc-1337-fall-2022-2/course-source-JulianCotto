8440ec696e2082279ebd2f03047e5734acc5fec6
JulianCotto <julian.cotto@g.austincc.edu>
2022-09-26 00:17:14 -0500

    Update decisions.cpp

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -2,17 +2,88 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
-//write include statement for decisions header
 
 using
-    std::this_thread::sleep_for,
-    std::chrono::milliseconds,
-    std::fixed,
+    std::this_thread::sleep_for, //for loading_bar()
+    std::chrono::milliseconds, //for loading_bar
     std::cout,
     std::cin;
 
+int menu()
+{
+    int selx; //(selx) selection variable initialized
 
-//Write code for function(s) code here
+    cout << "       MAIN MENU\n";
+    cout << "     -------------\n";
+    cout << "(1) Letter Grade Using If\n";
+    cout << "(2) Letter Grade Using Switch\n";
+    cout << "(3) Exit\n";
+    cin >> selx; //selx variable initialized by user
+    cout << '\n';
+
+    return selx; //user selx returned
+}
+
+void menu_switch_functions(int selx) //takes user selx variable as param
+{
+    string  ltgrade, //lettergrade
+            indart; //indefinitearticle
+    int grade = 0; //grade
+
+    switch (selx)
+    {
+        case 1:
+            menuFunSelxCase1(ltgrade, indart, grade); //call to functon that sets up if statement
+            break;
+
+        case 2:
+            menuFunSelxCase2(ltgrade, indart, grade); //call to functon that sets up switch statement
+            break;
+
+        case 3:
+            cout << "You have chosen to Exit\n";
+            break;
+
+        default:
+            cout << "You didn't make a valid selection\n";
+            cout << "\nPlease restart the program\n";
+    }
+}
+
+void menuFunSelxCase1(string ltgrade, string indart, int grade)
+{
+    cout << "Enter your grade (0-100)\n";
+    cin >> grade;
+
+    if (grade < 0 || grade > 100)
+    {
+        grade = -1;
+        ltgrade = get_letter_grade_using_if(grade);
+    }
+    else
+    {
+        ltgrade = get_letter_grade_using_if(grade);
+    }
+
+    loading_bar();
+
+    if (ltgrade == "A" || ltgrade == "F")
+    {
+        indart = " an ";
+        cout << "You got" << indart << ltgrade << "!\n";
+    }
+    else if (ltgrade == "B" || ltgrade == "C" || ltgrade == "D")
+    {
+        indart = " a ";
+        cout << "You got" << indart << ltgrade << "!\n";
+    }
+    else
+    {
+        cout << ltgrade;
+        cout << "\nPlease restart the program\n";
+    }
+}
+
 string get_letter_grade_using_if(int grade)
 {
     string ltGrade;
@@ -42,7 +113,41 @@ string get_letter_grade_using_if(int grade)
         ltGrade = "Invalid Entry. Grades are from 0 to 100\n";
     }
 
-return ltGrade;
+    return ltGrade;
+}
+
+void menuFunSelxCase2(string ltgrade, string indart, int grade)
+{
+    cout << "Enter your grade (0-100)\n";
+    cin >> grade;
+
+    if (grade < 0 || grade > 100)
+    {
+        grade = 110;
+        ltgrade = get_letter_grade_using_switch(grade);
+    }
+    else
+    {
+        ltgrade = get_letter_grade_using_switch(grade);
+    }
+
+    loading_bar();
+
+    if (ltgrade == "A" || ltgrade == "F")
+    {
+        indart = " an ";
+        cout << "You got" << indart << ltgrade << "!\n";
+    }
+    else if (ltgrade == "B" || ltgrade == "C" || ltgrade == "D")
+    {
+        indart = " a ";
+        cout << "You got" << indart << ltgrade << "!\n";
+    }
+    else
+    {
+        cout << ltgrade;
+        cout << "\nPlease restart the program\n";
+    }
 }
 
 string get_letter_grade_using_switch(int grade)
@@ -67,7 +172,7 @@ string get_letter_grade_using_switch(int grade)
         case 6:
             ltGrade = "D";
             break;
-
+        case 5:
         case 0:
             ltGrade = "F";
             break;
@@ -78,114 +183,17 @@ string get_letter_grade_using_switch(int grade)
     }
 
     return ltGrade;
-
 }
 
 void loading_bar()
 {
-    cout << "\nCalculating Grade";
+    cout << "Calculating Grade:";
     for (int i = 0; i < 3; i++)
     {
         sleep_for(milliseconds(1000) );
         cout << ".";
     }
     cout << "\n\n";
-}
-
-int menu()
-{
-    int selx;
-
-    cout << "       MAIN MENU\n";
-    cout << "     -------------\n";
-    cout << "(1) Letter Grade Using If\n";
-    cout << "(2) Letter Grade Using Switch\n";
-    cout << "(3) Exit\n";
-    cin >> selx;
-    cout << '\n';
-
-    return selx;
-}
-
-int menu_switch_functions(int selx)
-{
-    string  ltgrade,
-            indart;
-    int     grade;
-
-    switch (selx)
-    {
-
-
-        case 1:
-            cout << "Enter your grade (0-100)\n";
-            cin >> grade;
-            if (grade < 0 || grade > 100)
-            {
-                grade = -1;
-                ltgrade = get_letter_grade_using_if(grade);
-            }
-            else
-            {
-                ltgrade = get_letter_grade_using_if(grade);
-            }
-            loading_bar();
-            if (ltgrade == "A" || ltgrade == "F")
-            {
-                indart = " an ";
-                cout << "You got" << indart << ltgrade << "!\n";
-            }
-            else if (ltgrade == "B" || ltgrade == "C" || ltgrade == "D")
-            {
-                indart = " a ";
-                cout << "You got" << indart << ltgrade << "!\n";
-            }
-            else
-            {
-                cout << ltgrade;
-                cout << "\nPlease restart the program\n";
-            }
-            break;
-
-        case 2:
-            cout << "Enter your grade (0-100)\n";
-            cin >> grade;
-
-            if (grade < 0 || grade > 100)
-            {
-                grade = 110;
-                ltgrade = get_letter_grade_using_switch(grade);
-            }
-            else
-            {
-                ltgrade = get_letter_grade_using_switch(grade);
-            }
-            loading_bar();
-            if (ltgrade == "A" || ltgrade == "F")
-            {
-                indart = " an ";
-                cout << "You got" << indart << ltgrade << "!\n";
-            }
-            else if (ltgrade == "B" || ltgrade == "C" || ltgrade == "D")
-            {
-                indart = " a ";
-                cout << "You got" << indart << ltgrade << "!\n";
-            }
-            else
-            {
-                cout << ltgrade;
-                cout << "\nPlease restart the program\n";
-            }
-            break;
-
-        case 3:
-            cout << "You have chosen to Exit\n";
-            break;
-
-        default:
-            cout << "You didn't make a valid selection\n";
-            cout << "\nPlease restart the program\n";
-    }
 }
 
 void outro()
