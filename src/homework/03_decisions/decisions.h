4fb6fe7a7cc67e9d1f920131a7b2ae1511714553
JulianCotto <julian.cotto@g.austincc.edu>
2022-09-26 00:16:45 -0500

    Update decisions.h

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -2,18 +2,22 @@
 
 using std::string;
 
-//write include required statements
+//main menu and function calls to if and switch statements
+int menu();
+void menu_switch_functions(int selx); //calls to functions required in HW
 
+//switch case 1 call to if statement grade calculation
+void menuFunSelxCase1(string ltgrade, string indart, int grade);
 string get_letter_grade_using_if(int grade);
+
+//switch case 2 call to switch statement grade calculation
+void menuFunSelxCase2(string ltgrade, string indart, int grade);
 string get_letter_grade_using_switch(int grade);
 
-
-//Write a function(s) prototype  here
-
-int menu();
+//user experience functions
 void loading_bar();
 void outro();
-int menu_switch_functions(int selx);
+
 
 
 
