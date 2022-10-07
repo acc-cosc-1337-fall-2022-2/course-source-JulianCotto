b08b89e8f1e7972a56842e773face182c3cf9a84
JulianCotto <julian.cotto@g.austincc.edu>
2022-10-07 17:43:54 -0500

    Update func.h

--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,13 @@
+#include <string>
+using std::string;
+
 //add function(s) prototype here
+double get_gc_content(const string& dna);
+string get_dna_complement(string dna);
+string get_reverse_string(string dna);
+
+string to_lower(string dna);
+string to_upper(string dna);
+void menu();
+void exe_get_gc_content();
+void exe_get_dna_complement();
