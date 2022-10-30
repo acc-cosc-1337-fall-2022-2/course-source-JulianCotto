5bec5cc9330a5f2658f1182959b5aededd8b7df7
JulianCotto <julian.cotto@g.austincc.edu>
2022-10-29 23:55:12 -0500

    Update main.cpp

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,48 @@
+#include "tic_tac_toe.h"
+#include <iostream>
+
+using
+    std::cout,
+    std::cin;
 
 int main() 
 {
+    TicTacToe game;
 
-	return 0;
+    string first_player;
+    int
+        position,
+        selx = 0;
+
+
+    cout << "Tic Tac Toe\n";
+    cout << "Player 1: Choose X or O\n";
+    cin >> first_player;
+    game.start_game(first_player);
+    display_board_instructions();
+
+    while(selx == 0)
+    {
+        do
+        {
+            cout << "Player \'" << game.get_player() << "\' take your turn (Choose position on board)\n";
+            game.display_board();
+            cin >> position;
+            game.mark_board(position);
+        }
+        while(!game.game_over());
+
+        game.display_board();
+        game.game_over();
+        game.display_board();
+
+        cout << "Would you like to play again?\nPress 0: Yes\nPress 1: No\n";
+        cin >> selx;
+        if(selx == 0)
+        {
+           game.start_game(first_player);
+        }
+    }
+
+    return 0;
 }
