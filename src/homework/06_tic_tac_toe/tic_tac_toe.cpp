77b536fd90e55c3b19a92c386ac8d27c1f14c89e
JulianCotto <julian.cotto@g.austincc.edu>
2022-11-05 20:55:18 -0500

    Update tic_tac_toe.cpp

--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,7 +1,7 @@
 //cpp
 #include "tic_tac_toe.h"
 #include <iostream>
-#include <algorithm>
+
 
 using
     std::count,
@@ -9,7 +9,20 @@ using
 
 bool TicTacToe::game_over()
 {
-    return check_board_full();
+    if(check_column_win() || check_row_win() || check_diagonal_win())
+    {
+        set_winner();
+        return true;
+    }
+    else if (check_board_full())
+    {
+        winner = "C";
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 void TicTacToe::start_game(string first_player)
@@ -85,11 +98,112 @@ bool TicTacToe::check_board_full()
 
 void TicTacToe::clear_board()
 {
-    for(auto& peg : pegs)
+    for(auto & peg : pegs)
     {
         peg = " ";
     }
 }
+
+bool TicTacToe::check_column_win()
+{
+    bool truth;
+
+    if(pegs[0] == "X" && pegs[3] == "X" && pegs[6] == "X")
+    {
+        truth = true;
+    }
+    else if(pegs[1] == "X" && pegs[4] == "X" && pegs[7] == "X")
+    {
+        truth = true;
+    }
+    else if(pegs[2] == "X" && pegs[5] == "X" && pegs[8] == "X")
+    {
+        truth = true;
+    }
+    else if(pegs[0] == "O" && pegs[3] == "O" && pegs[6] == "O")
+    {
+        truth = true;
+    }
+    else if(pegs[1] == "O" && pegs[4] == "O" && pegs[7] == "O")
+    {
+        truth = true;
+    }
+    else if(pegs[2] == "O" && pegs[5] == "O" && pegs[8] == "O")
+    {
+        truth = true;
+    }
+    else
+        truth = false;
+
+    return truth;
+}
+
+bool TicTacToe::check_row_win()
+{
+    bool truth;
+
+    if(pegs[0] == "X" && pegs[1] == "X" && pegs[2] == "X")
+    {
+        truth = true;
+    }
+    else if(pegs[3] == "X" && pegs[4] == "X" && pegs[5] == "X")
+    {
+        truth = true;
+    }
+    else if(pegs[6] == "X" && pegs[7] == "X" && pegs[8] == "X")
+    {
+        truth = true;
+    }
+    else if(pegs[0] == "O" && pegs[1] == "O" && pegs[2] == "O")
+    {
+        truth = true;
+    }
+    else if(pegs[3] == "O" && pegs[4] == "O" && pegs[5] == "O")
+    {
+        truth = true;
+    }
+    else if(pegs[6] == "O" && pegs[7] == "O" && pegs[8] == "O")
+    {
+        truth = true;
+    }
+    else
+        truth = false;
+
+    return truth;
+}
+
+bool TicTacToe::check_diagonal_win()
+{
+    bool truth;
+
+    if(pegs[0] == pegs[4] && pegs[4] == pegs[8] && pegs[0] != " ")
+    {
+        truth = true;
+    }
+    else if(pegs[2] == pegs[4] && pegs[4] == pegs[6] && pegs[2] != " ")
+    {
+        truth = true;
+    }
+    else
+    {
+        truth = false;
+    }
+
+    return truth;
+}
+
+void TicTacToe::set_winner()
+{
+    if(TicTacToe::get_player() == "X")
+    {
+        winner = "O";
+    }
+    else
+    {
+        winner = "X";
+    }
+}
+
 
 //////////////////EXTRA FUNCTIONS//////////////////////////
 
@@ -99,3 +213,4 @@ void display_board_instructions()
     cout << "1|2|3 <---------------<|\n";
     cout << "4|5|6\n7|8|9\n";
 }
+
